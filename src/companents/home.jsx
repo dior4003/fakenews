6982c36bff7960d5df2786b4f53d8759be7e080a
dior4003 <diyor3933@gmail.com>
2022-03-10 22:29:46 +0500

    update

--- src/companents/home.jsx
+++ src/companents/home.jsx
@@ -21,8 +21,8 @@ export default function MyHome(){
                     <div className="card">
                       <div className="card-header d-flex justify-content-center ">
                         <div className="overlay">
-                          <h4>Project name</h4>
-                          <p>Project description</p>
+                          <h4>MERN Stack</h4>
+                          <p>Mongo-DB ExpressJs ReactJs NodeJs</p>
                         </div>
                         <img src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/145060451/original/faa00a6c4e32b667c60827e8afd66e76f0f69a77/build-your-full-stack-application-for-you.jpg" alt="Portfolio img"/>
                       </div>
@@ -32,8 +32,8 @@ export default function MyHome(){
                     <div className="card">
                       <div className="card-header d-flex justify-content-center">
                         <div className="overlay">
-                          <h4>Project name</h4>
-                          <p>Project description</p>
+                          <h4>Mongo DB</h4>
+                          <p>Bg and Free Data Base</p>
                         </div>
                         <img src="https://yt3.ggpht.com/ytc/AKedOLR1ULhkE1KlBnOZi9RLmi2y0zskVvjom1YWwA8S=s900-c-k-c0x00ffffff-no-rj" alt="Portfolio img"/>
                       </div>
@@ -43,8 +43,8 @@ export default function MyHome(){
                     <div className="card">
                       <div className="card-header d-flex justify-content-center">
                         <div className="overlay">
-                          <h4>Project name</h4>
-                          <p>Project description</p>
+                          <h4>ExpressJS</h4>
+                          <p>For Back-End Free Library</p>
                         </div>
                         <img src="https://avatars.mds.yandex.net/i?id=0749b10c5fb44010e9b7c04fac397a23-5910048-images-thumbs&n=13" alt="Portfolio img"/>
                       </div>
@@ -56,8 +56,8 @@ export default function MyHome(){
                     <div className="card">
                       <div className="card-header d-flex justify-content-center">
                         <div className="overlay">
-                          <h4>Project name</h4>
-                          <p>Project description</p>
+                          <h4>ReactJS</h4>
+                          <p>ReactJs The web creates great software</p>
                         </div>
                         <img src="https://avatars.mds.yandex.net/i?id=33c413e451627d30c268003771d59a04-5235821-images-thumbs&n=13" alt="Portfolio img"/>
                       </div>
@@ -67,8 +67,8 @@ export default function MyHome(){
                     <div className="card">
                       <div className="card-header d-flex justify-content-center">
                         <div className="overlay">
-                          <h4>Project name</h4>
-                          <p>Project description</p>
+                          <h4>Node JS</h4>
+                          <p>It forms the basis of your web page</p>
                         </div>
                         <img src="https://www.crmdevelopmentcompany.com/wp-content/uploads/2020/09/Node_logo_NodeJS0-1-800x643.png" alt="Portfolio img"/>
                       </div>
@@ -78,8 +78,8 @@ export default function MyHome(){
                     <div className="card">
                       <div className="card-header d-flex justify-content-center">
                         <div className="overlay">
-                          <h4>Project name</h4>
-                          <p>Project description</p>
+                          <h4>NextJs</h4>
+                          <p>FrameWork Node JS</p>
                         </div>
                         <img src="https://react-etc.net/files/2017-08/next-js-30.png" alt="Portfolio img"/>
                       </div>
